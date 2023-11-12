f050f76e4c8c59c7bda34c9aeb6f43eea7243048
astapay <astapay@mail.bradley.edu>
2023-11-12 13:54:50 -0600

    playtest build
    yay

--- Texas Hell'd Em/Assets/Scripts/MenuScripts/ButtonScript.cs
+++ Texas Hell'd Em/Assets/Scripts/MenuScripts/ButtonScript.cs
@@ -34,7 +34,6 @@ public class ButtonScript : MonoBehaviour
     public void QuitButton()
     {
         Application.Quit();
-        UnityEditor.EditorApplication.isPlaying = false;
     }
 
     // <summary>
