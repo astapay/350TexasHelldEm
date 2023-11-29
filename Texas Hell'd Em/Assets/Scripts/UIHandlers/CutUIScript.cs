321c0493ccae0c1a7acea3346ce84f7728f0cc56
ZarkosHQ <123423925+ZarkosHQ@users.noreply.github.com>
2023-11-29 14:27:59 -0600

    Some cut scene set up

--- Texas Hell'd Em/Assets/Scripts/UIHandlers/CutUIScript.cs
+++ Texas Hell'd Em/Assets/Scripts/UIHandlers/CutUIScript.cs
@@ -10,6 +10,8 @@ summary: this is the script that is used swap out the ui for the cutscene when c
 */
 public class CutUiScript : MonoBehaviour
 {
+    GameController gameController;
+
     //player cards
     [SerializeField] private GameObject playerCard1;
     [SerializeField] private GameObject playerCard2;
@@ -30,64 +32,82 @@ public class CutUiScript : MonoBehaviour
     [SerializeField] private GameObject bot3Card1;
     [SerializeField] private GameObject bot3Card2;
 
-    public void SwapSprite(int card, Sprite sprite)
+    //player indicator
+    [SerializeField] private GameObject PlayerIndicator;
+    //npc indicators
+    [SerializeField] private GameObject NPC1Indicator;
+    [SerializeField] private GameObject NPC2Indicator;
+    [SerializeField] private GameObject NPC3Indicator;
+
+    //player card sprites
+    private Sprite playerCard1Sprite;
+    private Sprite playerCard2Sprite;
+    //river card sprites
+    private Sprite riverCard1Sprite;
+    private Sprite riverCard2Sprite;
+    private Sprite riverCard3Sprite;
+    private Sprite riverCard4Sprite;
+    private Sprite riverCard5Sprite;
+
+    //npc1 card sprites
+    private Sprite bot1Card1Sprite;
+    private Sprite bot1Card2Sprite;
+    //npc2 card sprites
+    private Sprite bot2Card1Sprite;
+    private Sprite bot2Card2Sprite;
+    //npc3 card sprites
+    private Sprite bot3Card1Sprite;
+    private Sprite bot3Card2Sprite;
+
+    //player indicator sprites
+    private Sprite PlayerIndicatorCard;
+    private Sprite PlayerIndicatorSuit;
+    private Sprite PlayerIndicatorHand;
+
+    //npc indicator sprites
+    private Sprite NPC1IndicatorCard;
+    private Sprite NPC1IndicatorSuit;
+    private Sprite NPC1IndicatorHand;
+
+    private Sprite NPC2IndicatorCard;
+    private Sprite NPC2IndicatorSuit;
+    private Sprite NPC2IndicatorHand;
+
+    private Sprite NPC3IndicatorCard;
+    private Sprite NPC3IndicatorSuit;
+    private Sprite NPC3IndicatorHand;
+
+    private void Start()
     {
-        if (card == 0)
-        {
-            playerCard1.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 1)
-        {
-            playerCard2.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 2)
-        {
-            riverCard1.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 3)
-        {
-            riverCard2.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 4)
-        {
-            riverCard3.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 5)
-        {
-            riverCard4.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 6)
-        {
-            riverCard5.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 7)
-        {
-            bot1Card1.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 8)
-        {
-            bot1Card2.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 9)
-        {
-            bot2Card1.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 10)
-        {
-            bot2Card2.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 11)
-        {
-            bot3Card1.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        else if (card == 12)
-        {
-            bot3Card2.GetComponent<SpriteRenderer>().sprite = sprite;
-        }
-        //delete before release---------------------------------------------------------------------------------------
-        else
-        {
-            Debug.Log(card);
-        }
+        //get cards from game
+        GetCardSprites();
+        //set player cards
+        playerCard1.GetComponent<SpriteRenderer>().sprite = playerCard1Sprite;
+        playerCard2.GetComponent<SpriteRenderer>().sprite = playerCard2Sprite;
+        //set river cards
+        riverCard1.GetComponent<SpriteRenderer>().sprite = riverCard1Sprite;
+        riverCard2.GetComponent<SpriteRenderer>().sprite = riverCard2Sprite;
+        riverCard3.GetComponent<SpriteRenderer>().sprite = riverCard3Sprite;
+        riverCard4.GetComponent<SpriteRenderer>().sprite = riverCard4Sprite;
+        riverCard5.GetComponent<SpriteRenderer>().sprite = riverCard5Sprite;
+
+    }
+
+    private void GetCardSprites()
+    {
+        Sprite[] spriteSheet = gameController.GetCardSprites();
+        playerCard1Sprite = spriteSheet[1];
+        playerCard2Sprite = spriteSheet[2];
+        riverCard1Sprite = spriteSheet[3];
+        riverCard2Sprite = spriteSheet[4];
+        riverCard3Sprite = spriteSheet[5];
+        riverCard4Sprite = spriteSheet[6];
+        riverCard5Sprite = spriteSheet[7];
+        bot1Card1Sprite = spriteSheet[8];
+        bot1Card2Sprite = spriteSheet[9];
+        bot2Card1Sprite = spriteSheet[10];
+        bot2Card2Sprite = spriteSheet[11]; 
+        bot3Card1Sprite = spriteSheet[12];
+        bot3Card2Sprite = spriteSheet[13];
     }
 }
